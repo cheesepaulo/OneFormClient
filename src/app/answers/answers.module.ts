9f6e0f4da16928c463cf6d2e046710744cb9d632
Paulo Cesar <pcdsjr@gmail.com>
2017-11-18 09:22:48 -0500

    generate scaffold for graph components

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AswerListComponent } from './aswer-list/aswer-list.component';
 import { AswerNewComponent } from './aswer-new/aswer-new.component';
+import { GraphNumberComponent } from './shared/graph-number/graph-number.component';
+import { GraphTextComponent } from './shared/graph-text/graph-text.component';
+import { GraphBooleanComponent } from './shared/graph-boolean/graph-boolean.component';
+import { IndividualDetailsComponent } from './shared/individual-details/individual-details.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AswerListComponent, AswerNewComponent]
+  declarations: [AswerListComponent, AswerNewComponent, GraphNumberComponent, GraphTextComponent, GraphBooleanComponent, IndividualDetailsComponent]
 })
 export class AnswersModule { }
